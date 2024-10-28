6caf8dce3363fb7ee0b6549114d80ecf30f4391e
RH0128 <79930122+RH0128@users.noreply.github.com>
2024-10-28 18:41:51 +0900

    初回ファイルアップロード

---
+++ js/tailwind.config.js
@@ -0,0 +1,8 @@
+export const theme = {
+    extend: {
+        fontFamily: {
+            japanese: ['Kosugi', 'sans-serif'], // 日本語用フォント
+            latin: ['Pattaya', 'sans-serif'], // 欧文用フォント
+        },
+    },
+};
